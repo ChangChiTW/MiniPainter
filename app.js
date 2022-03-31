a131467854ce1d5480147212759281e68a769199
108061250 <sam54537@gmail.com>
2022-03-31 16:59:37 +0800

    feat: change mode

--- app.js
+++ app.js
@@ -10,41 +10,53 @@ function resize() {
 
 window.addEventListener("resize", resize);
 
-$(".toggle").on("click", function () {
-  $(".toggle").removeClass("active");
-  $(this).addClass("active");
+$(document).ready(function () {
+  let mode = "pencil";
+  let pos = { x: 0, y: 0 };
+  const lineCap = ["butt", "round", "square"];
+
+  $(".toggle").on("click", function () {
+    $(".toggle").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $("button").on("click", function () {
+    console.log($(this).attr("id"));
+    mode = $(this).attr("id");
+  });
+
+  document.addEventListener("mousedown", setPosition);
+  document.addEventListener("mouseenter", setPosition);
+
+  document.addEventListener("mousemove", draw);
+
+  document.getElementById("clear").addEventListener("click", function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  function setPosition(e) {
+    pos.x = e.clientX;
+    pos.y = e.clientY - window.innerHeight * 0.1;
+  }
+
+  function draw(e) {
+    if (e.buttons !== 1) return;
+    if (mode === "pencil") {
+      // const color = document.getElementById("hex").value;
+      // const lineWidth = document.getElementById("lineWidth").value;
+      // const lineShape = document.getElementById("lineShape").value;
+
+      ctx.beginPath();
+
+      ctx.lineWidth = 10;
+      ctx.lineCap = lineCap[1];
+      // ctx.strokeStyle = color;
+
+      ctx.moveTo(pos.x, pos.y);
+      setPosition(e);
+      ctx.lineTo(pos.x, pos.y);
+
+      ctx.stroke();
+    }
+  }
 });
-
-document.addEventListener("mousemove", draw);
-document.addEventListener("mousedown", setPosition);
-document.addEventListener("mouseenter", setPosition);
-document.getElementById("clear").addEventListener("click", function () {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
-
-let pos = { x: 0, y: 0 };
-const lineCap = ["butt", "round", "square"];
-
-function setPosition(e) {
-  pos.x = e.clientX;
-  pos.y = e.clientY - window.innerHeight * 0.1;
-}
-
-function draw(e) {
-  if (e.buttons !== 1) return;
-  // const color = document.getElementById("hex").value;
-  // const lineWidth = document.getElementById("lineWidth").value;
-  // const lineShape = document.getElementById("lineShape").value;
-
-  ctx.beginPath();
-
-  ctx.lineWidth = 10;
-  ctx.lineCap = lineCap[1];
-  // ctx.strokeStyle = color;
-
-  ctx.moveTo(pos.x, pos.y);
-  setPosition(e);
-  ctx.lineTo(pos.x, pos.y);
-
-  ctx.stroke();
-}
